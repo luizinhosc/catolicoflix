b8d82b3edb9bbf4a701b52538ac2d43418853882
DA COSTA <luiz.anselmo@dxc.com>
2020-08-04 22:27:12 -0300

    inicial

---
+++ src/components/Menu/components/BannerMain/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import VideoIframeResponsive from './components/VideoIframeResponsive';
+import { BannerMainContainer, ContentAreaContainer, WatchButton } from './styles';
+
+function getYouTubeId(youtubeURL) {
+  return youtubeURL
+    .replace(
+      /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
+      '$7',
+    );
+}
+
+export default function BannerMain({
+  videoTitle,
+  videoDescription,
+  url,
+}) {
+  const youTubeID = getYouTubeId(url);
+  console.log(youTubeID);
+  
+ // const bgUrl = `https://img.youtube.com/vi/${youTubeID}/maxresdefault.jpg`;
+  const bgUrl = `https://www.sintonizeaqui.com.br/wp-content/uploads/2017/11/Par%C3%B3quia-Cristo-Redentor-Nossa-Senhora-das-Gra%C3%A7as-5-1280x720.jpg`
+
+  return (
+    <BannerMainContainer backgroundImage={bgUrl}>
+      <ContentAreaContainer>
+        <ContentAreaContainer.Item>
+          <ContentAreaContainer.Title>
+            {videoTitle}
+          </ContentAreaContainer.Title>
+
+          <ContentAreaContainer.Description>
+            {videoDescription}
+          </ContentAreaContainer.Description>
+        </ContentAreaContainer.Item>
+
+        <ContentAreaContainer.Item>
+          <VideoIframeResponsive
+            youtubeID={youTubeID}
+          />
+          <WatchButton>
+            Assistir
+          </WatchButton>
+        </ContentAreaContainer.Item>
+      </ContentAreaContainer>
+    </BannerMainContainer>
+  );
+}
